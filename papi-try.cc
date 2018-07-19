5b04647fa5f20f658a088c53951eedd54ce7b9ef
Qing Zheng <zhengq@cs.cmu.edu>
2018-07-19 13:43:23 -0400

    change rand() to murmur hash

--- papi-try.cc
+++ papi-try.cc
@@ -52,6 +52,57 @@
 /* max events to monitor */
 #define MAX_EVENTS 16
 
+//-----------------------------------------------------------------------------
+// MurmurHash2, 64-bit versions, by Austin Appleby
+//
+// 64-bit hash for 64-bit platforms
+static uint64_t murmurhash64(const void* key, int len, uint64_t seed) {
+  const uint64_t m = 0xc6a4a7935bd1e995LLU;
+  const int r = 47;
+
+  uint64_t h = seed ^ (len * m);
+
+  const uint64_t* data = (const uint64_t*)key;
+  const uint64_t* end = data + (len / 8);
+
+  while (data != end) {
+    uint64_t k = *data++;
+
+    k *= m;
+    k ^= k >> r;
+    k *= m;
+
+    h ^= k;
+    h *= m;
+  }
+
+  const unsigned char* data2 = (const unsigned char*)data;
+
+  switch (len & 7) {
+    case 7:
+      h ^= uint64_t(data2[6]) << 48;
+    case 6:
+      h ^= uint64_t(data2[5]) << 40;
+    case 5:
+      h ^= uint64_t(data2[4]) << 32;
+    case 4:
+      h ^= uint64_t(data2[3]) << 24;
+    case 3:
+      h ^= uint64_t(data2[2]) << 16;
+    case 2:
+      h ^= uint64_t(data2[1]) << 8;
+    case 1:
+      h ^= uint64_t(data2[0]);
+      h *= m;
+  };
+
+  h ^= h >> r;
+  h *= m;
+  h ^= h >> r;
+
+  return h;
+}
+
 /*
  * helper/utility functions, included inline here so we are self-contained
  * in one single source file...
@@ -363,7 +414,7 @@ static int runops(size_t sz) {
   } else {
     t = PAPI_get_real_usec();
     for (size_t i = 0; i < sz; i++) {
-      mem[rand() % sz]++;
+      mem[murmurhash64(&i, sizeof(i), myrank) % sz]++;
     }
     t = PAPI_get_real_usec() - t;
     printf(">> %d MiB: %.3f msec\n", int(sz >> 20), 1.0 * t / 1000);
